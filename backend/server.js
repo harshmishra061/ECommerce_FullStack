c5f693fa03b665c32ad1f5e65aa8b057851d3657
Harsh Mishra <harsh.mishra@pratishthanventures.com>
2024-09-28 16:33:33 +0530

    added error handling

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,29 @@ const app = require('./app');
 const dotenv = require('dotenv');
 const connectDatabase = require('./config/database')
 
+// handling uncaught exception
+process.on('uncaughtException', (err) => {
+    console.log(`Error : ${err.message}`);
+    console.log('Shuting down the server due to uncaught Exception');
+    process.exit(1);
+})
+
+
+
 dotenv.config({ path: "backend/config/config.env" });
 // connecting database
 connectDatabase();
 
 app.listen(process.env.PORT, () => {
     console.log('server is working on', process.env.PORT);
+})
+
+// Unhandled Promise Rejection
+process.on('unhandledRejection', err => {
+    console.log(`Error : ${err.message}`);
+    console.log('Shuting down the server due to unhandled promise rejection');
+    server.close(() => {
+        process.exit(1);
+    })
+
 })
